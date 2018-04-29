b2bb9c07655bac21299261885ca3563b7895751a
yvetteyuanqin <lilyoorainqy@gmail.com>
2018-04-29 13:53:44 -0700

    test

---
+++ pro3/wifitest/wifitest.ino
@@ -0,0 +1,18 @@
+#include <ESP8266WiFi.h>
+ 
+void setup(){
+ 
+   Serial.begin(115200);
+   delay(500);
+ 
+   Serial.println();
+   Serial.print("MAC: ");
+   Serial.println(WiFi.macAddress());
+ 
+}
+ 
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
